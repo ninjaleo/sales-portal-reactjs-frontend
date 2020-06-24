89109ad0ef801c25c6dfed37f233211963cccd35
ninjaleo <63445543+ninjaleo@users.noreply.github.com>
2020-06-24 23:37:11 +0100

    Update loginvalidator.js

--- src/data/loginvalidator.js
+++ src/data/loginvalidator.js
@@ -5,7 +5,7 @@ function loginValidator(logincredentials) {
     console.log(logincredentials);
     return dispatch => {
         dispatch(loginPending());
-        axios.post('http://backend:3001/login', logincredentials)
+        axios.post('http://localhost:3001/login', logincredentials)
             .then(
                 response => {
                     const { status } = response.data;
