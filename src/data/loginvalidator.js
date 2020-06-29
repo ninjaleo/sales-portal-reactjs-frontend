161d296575fb96f78931d6964abc10a821bb3170
ninjaleo <63445543+ninjaleo@users.noreply.github.com>
2020-06-29 18:43:57 +0100

    Update loginvalidator.js

--- src/data/loginvalidator.js
+++ src/data/loginvalidator.js
@@ -5,7 +5,7 @@ function loginValidator(logincredentials) {
     console.log(logincredentials);
     return dispatch => {
         dispatch(loginPending());
-        //axios.post('http://node-service/login', logincredentials)
+        //axios.post('http://35.232.237.232/login', logincredentials)
         axios.post('http://node-service/', logincredentials)
             .then(
                 response => {
