b0c80292b73faa2afa70daeb0e5385053b93c5d5
sureshtripini <sureshtripini@gmail.com>
2020-06-29 20:42:45 +0100

    firebase login changes

--- src/reducers/loginreducer.js
+++ src/reducers/loginreducer.js
@@ -1,37 +1,42 @@
-const LOGIN_PENDING = 'LOGIN_PENDING';
-const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-const LOGIN_FAILED = 'LOGIN_FAILED';
+import { loginSuccess, loginFailed } from "../actions/userAction";
 
-const loginreducer = (state = { loginStatus: "NONE" }, action) => {
-    // write Reducers to handle the actions.
-    console.log("action name:" + action.payload);
-    switch (action.type) {
-        case LOGIN_PENDING:
-            return (
-                {
-                    userName: action.payload,
-                    isLoggedIn: false,
-                    loginStatus: "PENDING"
-                });
-        case LOGIN_SUCCESS:
-            return (
-                {
-                    userName: action.payload,
-                    isLoggedIn: true,
-                    loginStatus: "SUCCESS"
-                });
-        case LOGIN_FAILED:
+const LOGIN_PENDING = "LOGIN_PENDING";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGIN_FAILED = "LOGIN_FAILED";
 
-            return (
-                {
-                    message: action.payload,
-                    isLoggedIn: false,
-                    loginStatus: "FAILED"
-                });
-        default:
-            return state;
+export const loginreducer = (state = { loginStatus: "NONE" }, action) => {
+  // write Reducers to handle the actions.
+  console.log("action name:" + action.payload);
+  switch (action.type) {
+    case LOGIN_PENDING:
+      return {
+        userName: action.payload,
+        isLoggedIn: false,
+        loginStatus: "PENDING",
+      };
+    case LOGIN_SUCCESS:
+      return {
+        userName: action.payload,
+        isLoggedIn: true,
+        loginStatus: "SUCCESS",
+      };
+    case LOGIN_FAILED:
+      return {
+        message: action.payload,
+        isLoggedIn: false,
+        loginStatus: "FAILED",
+      };
+    default:
+      return state;
+  }
+};
 
+export function setGoogleLogin(user) {
+  return (dispatch) => {
+    if (user && user.emailVerified) {
+      dispatch(loginSuccess(user.displayName));
+    } else {
+      dispatch(loginFailed("Invalid Google User"));
     }
+  };
 }
-
-export default loginreducer;
